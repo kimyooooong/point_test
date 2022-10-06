b22aa4c14ef636d6e90a58e839346cb04621ff45
DESKTOP-1BJU96Q\user <kimkim8371@gmailcom>
2022-10-06 18:12:51 +0900

    최초커밋

---
+++ src/main/java/point/test/PointTestApplication.java
@@ -0,0 +1,13 @@
+package point.test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PointTestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PointTestApplication.class, args);
+    }
+
+}

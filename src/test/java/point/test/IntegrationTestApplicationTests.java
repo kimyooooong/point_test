814988d7378be1e6080b9c5ef00552234d8633ac
DESKTOP-1BJU96Q\user <kimkim8371@gmailcom>
2022-10-08 16:09:00 +0900

    테스트코드 작성중

--- src/test/java/point/test/IntegrationTestApplicationTests.java
+++ src/test/java/point/test/IntegrationTestApplicationTests.java
@@ -1,12 +1,16 @@
 package point.test;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.util.Assert;
 import point.test.common.Const;
 import point.test.domain.Member;
 import point.test.repository.MemberRepository;
 import point.test.service.MemberService;
+import point.test.service.PointService;
 
 import javax.transaction.Transactional;
 
@@ -14,20 +18,24 @@ import javax.transaction.Transactional;
 class IntegrationTestApplicationTests {
 
     @Autowired
-    MemberRepository memberRepository;
+    MemberService memberService;
 
     @Autowired
-    MemberService memberService;
+    PointService pointService;
 
     @Test
     @Transactional
-    void contextLoads() {
-
-       Member member = memberService.getMember(Const.TEST_MEMBER_ID);
-       System.out.println(member);
-
+    void 포인트_세이브_테스트() {
 
 
     }
+    
+    @Test
+    @Transactional
+    void 포인트_사용_테스트(){
+
+
+    }
+    
 
 }
--- src/test/java/point/test/ServiceTestApplicationTests.java
+++ src/test/java/point/test/ServiceTestApplicationTests.java
@@ -1,5 +1,6 @@
 package point.test;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -7,6 +8,7 @@ import point.test.common.Const;
 import point.test.domain.Member;
 import point.test.repository.MemberRepository;
 import point.test.service.MemberService;
+import point.test.service.PointService;
 
 import javax.transaction.Transactional;
 
@@ -14,20 +16,69 @@ import javax.transaction.Transactional;
 class ServiceTestApplicationTests {
 
     @Autowired
-    MemberRepository memberRepository;
+    MemberService memberService;
 
     @Autowired
-    MemberService memberService;
+    PointService pointService;
 
     @Test
     @Transactional
-    void contextLoads() {
+    void 포인트_세이브_테스트() {
+        System.out.println("===========포인트_세이브_테스트 시작============");
 
-       Member member = memberService.getMember(Const.TEST_MEMBER_ID);
-       System.out.println(member);
+        Long amount = 50000L;
+
+        pointService.savePoint(Const.TEST_MEMBER_ID , amount , "공짜 포인트");
+        Assertions.assertEquals(pointService.getTotalPoint(Const.TEST_MEMBER_ID) , amount);
+
+        System.out.println("===========포인트_세이브_테스트 종료============");
+    }
+
+    @Test
+    @Transactional
+    void 포인트_사용_테스트(){
+
+        System.out.println("===========포인트_사용_테스트 시작============");
+
+        Long amount = 50000L;
+
+        System.out.println("포인트 충전 전 : " + pointService.getTotalPoint(Const.TEST_MEMBER_ID) );
+
+        pointService.savePoint(Const.TEST_MEMBER_ID , amount , "공짜 포인트");
+
+        System.out.println("포인트 사용 전 : " + pointService.getTotalPoint(Const.TEST_MEMBER_ID) );
+
+        amount -= 25000;
+
+        pointService.usingPoint(Const.TEST_MEMBER_ID , amount , "공짜 포인트 사용");
+
+        System.out.println("포인트 사용 후 : " + pointService.getTotalPoint(Const.TEST_MEMBER_ID) );
+
+        Assertions.assertEquals(pointService.getTotalPoint(Const.TEST_MEMBER_ID) , amount);
+
+        System.out.println("===========포인트_사용_테스트 종료============");
+
+    }
+
+    @Test
+    @Transactional
+    void 포인트_사용_취소_테스트(){
+        System.out.println("===========포인트_사용_취소_테스트 시작============");
 
 
 
+
+
+
+
+
+
+
+
+
+
+
+        System.out.println("===========포인트_사용_취소_테스트 종료============");
     }
 
 }
